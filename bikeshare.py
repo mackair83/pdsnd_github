add6f5d8d61a49c4824510b944e09979aeb94a12
David Thanh Tran <dttran83@hotmail.com>
2022-07-19 01:48:04 -0500

    This commit will change line 22 city

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input("Would you like to see data for Chicago, New York City, or Washington?\n").lower()
-        if city in CITY_DATA :
+        if city in ['chicago', 'new your city', 'washington'] :
                 print('\ncity:{}\n'.format(city))
                 break
         else:
